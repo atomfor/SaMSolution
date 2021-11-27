f6c0c1de8162d3f2d2df23c15fed6737358e62f8
oksana <oksanakatoli8290@gmail.com>
2021-11-27 15:57:34 +0200

    Made new theme - Lamoda

--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -19,6 +19,18 @@
  *
  */
 module.exports = {
+    lamoda: {
+        area: 'frontend',
+        name: 'Oksana/lamoda',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
     themestart: {
         area: 'frontend',
         name: 'Oksana/themestarts',
