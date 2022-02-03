9a82acdfb7c9a3402cde66ca83de5c1f969ee3ff
oksana <oksanakatoli8290@gmail.com>
2022-02-03 21:46:54 +0200

    Translation, language packages, custom-widget

--- app/etc/config.php
+++ app/etc/config.php
@@ -354,6 +354,7 @@ return [
         'Klarna_Kp' => 1,
         'Klarna_Onsitemessaging' => 1,
         'Klarna_KpGraphQl' => 1,
+        'Oksana_SampleWidget' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
